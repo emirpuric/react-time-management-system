c5338e591328a00b499113b17e5b8234b893fbfe
Emir Puric <emir.puric@gmail.com>
2020-04-04 23:03:09 +0200

    Connect API with MongoDB and add Mongoose object modeling tool

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,15 +1,39 @@
 import express from 'express';
 
+require('dotenv').config();
+
 const PORT = 5000;
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true,  useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(PORT, () => {
     console.log(`Time Management System API server running on port ${PORT}`)
 });
 
+app.use(express.json());
+
+//TODO REMOVE
+const Test = require("./model/test");
+
 app.get('/', (req, res) => {
-    res.status(200).send({
-        success: true,
-        message: 'Hello World!'
+    Test.find({}, (error, tests) => {
+        console.log(JSON.stringify(tests));
+        res.status(200).send(tests);
     });
+});
+
+app.post('/', async (req, res) => {
+    let test = new Test({
+        name: req.body.name
+    });
+    try {
+        await test.save();
+        res.status(201).send(test);
+    } catch(error) {
+        res.status(400).send(error);
+    }
 });
